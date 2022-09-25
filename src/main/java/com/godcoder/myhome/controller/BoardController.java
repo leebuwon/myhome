8162b91ee3ff96eae1ce4a7aaf2fb17402b74c1f
leebuwon <bw1611@naver.com>
2022-09-25 21:07:09 +0900

    8 commit

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -2,12 +2,16 @@ package com.godcoder.myhome.controller;
 
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,10 +21,35 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
+        Page<Board> boards = boardRepository.findAll(PageRequest.of(0, 20));
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id == null){
+            model.addAttribute("board", new Board());
+        }else {
+            Board board = boardRepository.findById(id).orElse(null); // optional 이기 때문에 조회된 값이 없을 경우 null로 반환
+            model.addAttribute("board", board);
+        }
+//         model.addAttribute("board", new Board());
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data       // lombok을 이용하여 getter / setter 없이 사용가능
@@ -13,6 +15,8 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ATUO는 3가지 중 알아서 선택해줌
     private Long id;
+    @NotNull
+    @Size(min=2, max=30)
     private String title;
     private String content;
 
