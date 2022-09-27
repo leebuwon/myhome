e80bd6ee08e216ffe3769cde45beff2e6e0d75e3
Kwangsun Noh <nohkwangsun@gmail.com>
2022-09-27 20:22:56 +0800

    update

--- src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
@@ -2,15 +2,18 @@ package com.godcoder.myhome.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Autowired
@@ -52,4 +55,8 @@ public class WebSecurityConfig {
                         + "inner join role r on ur.role_id = r.id "
                         + "where username = ?");
     }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
--- src/main/java/com/godcoder/myhome/config/WebSecurityConfigurerAdapterImpl.java
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfigurerAdapterImpl.java
@@ -9,12 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class WebSecurityConfigurerAdapterImpl extends WebSecurityConfigurerAdapter {
 
     @Autowired
