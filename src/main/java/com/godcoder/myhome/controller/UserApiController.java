00cbea754bd5ee4ea3c5ad2f597248f8f6938802
leebuwon <bw1611@naver.com>
2022-09-29 17:59:34 +0900

    sql check commit

--- src/main/java/com/godcoder/myhome/controller/UserApiController.java
+++ src/main/java/com/godcoder/myhome/controller/UserApiController.java
@@ -1,8 +1,9 @@
-
 package com.godcoder.myhome.controller;
+
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.model.User;
 import com.godcoder.myhome.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 class UserApiController {
 
     @Autowired
@@ -18,7 +20,12 @@ class UserApiController {
 
     @GetMapping("/users")
     List<User> all() {
-        return repository.findAll();
+        List<User> users = repository.findAll();
+        log.debug("getBoards().size() 호출전");
+        log.debug("getBoards().size() : {}", users.get(1).getBoards().size());
+        log.debug("getBoards().size() 호출후");
+//        users.get(0).getBoards().size();
+        return users;
     }
 
     @PostMapping("/users")
--- src/main/java/com/godcoder/myhome/model/User.java
+++ src/main/java/com/godcoder/myhome/model/User.java
@@ -21,13 +21,17 @@ public class User {
     private Boolean enabled;
 
     @ManyToMany
+    @JsonIgnore
     @JoinTable(
             name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles = new ArrayList<>();
     
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)   // user가 board를 조회할 수 있게 양방향 mapping을 하였음
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)   // user가 board를 조회할 수 있게 양방향 mapping을 하였음
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+//    @JsonIgnore
     private List<Board> boards = new ArrayList<>();
 
+    // one으로 끝나는 것은 기본값이 EAGER / Many로 끝나는 것은 기본 값이 LAZY
 }
